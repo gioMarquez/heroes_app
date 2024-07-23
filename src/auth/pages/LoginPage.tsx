2179b00715f7d2cd6513d4780a1285cb19e86a96
Jhovanny <jhovanny.fragoso@codiceglobal.com>
2024-07-23 11:49:57 -0600

    Style(buttonLogin)

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -10,16 +10,20 @@ const LoginPage = () => {
 	};
 
 	return (
-		<div className=" m-5">
-			<h1 className="text-3xl ">Login</h1>
+		<div className=" m-5 bg-white">
+			<h1 className="text-3xl font-thin">Login</h1>
 			<hr />
 
 			<div className="mb-4"></div>
+
 			<button
-				className="bg-blue-700 rounded-2xl h-[50px] w-[200px] text-white font-bold text-xl hover:bg-blue-700/80 hover:duration-75"
 				onClick={() => onLogin()}
+				className="btn relative inline-flex items-center justify-start overflow-hidden font-medium transition-all bg-indigo-100 rounded hover:bg-white group py-1.5 px-2.5 w-48 "
 			>
-				Login
+				<span className="w-56 h-48 rounded bg-indigo-600 absolute bottom-0 left-0 translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+				<span className="relative w-full text-center text-indigo-600 transition-colors duration-300 ease-in-out group-hover:text-white">
+					Login
+				</span>
 			</button>
 		</div>
 	);
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -5,12 +5,12 @@ import HeroesRoutes from "../heores/routes/HeroesRoutes";
 
 const AppRouter = () => {
 	return (
-		<>
+		<div className="bg-white">
             <Routes>
                 <Route path="login" element={ <LoginPage />} />
                 <Route path="/*" element={ <HeroesRoutes />} />
             </Routes>
-		</>
+		</div>
 	);
 };
 
